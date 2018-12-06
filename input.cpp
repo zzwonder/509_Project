626963b70c8e0d02b66e1ee6cbb534fa8f0a7ef9
zzwonder <zzwonder@users.noreply.github.com>
2018-12-06 17:20:42 -0600

    Revert "Revert "Initial commit""
    This reverts commit e25c3b7b12b24d3e3a9fea4379a0de08dfc167cc.

---
+++ input.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+using namespace std;
+struct datasize Dsize;
+struct variable Vars[MAXNV];
+struct clause Clauses[MAXNC];
+void read_case(string firename) {
+	ifstream fin;
+	fin.open(firename);
+	if (!fin) {
+		cout << "fail to open the file" << endl;
+	}
+	char command;
+	int snum = 0;
+	for (int i = 1; i < MAXNV; i++) {
+		Vars[i] = variable(i);
+	}
+	while (fin >> command) {
+		if (command == 'c') {
+			char ign[100];
+			fin.getline(ign,100);
+	//		cout << ign << endl;
+		}
+		else if (command == 'p') {
+			fin >> Dsize.type >> Dsize.nvars >> Dsize.nclauses;
+		}
+		else if (command == 's') {
+			snum++;
+			int varno;
+			fin >> varno;
+			while (varno != 0) {
+				Clauses[snum].vars.push_back(varno);
+				Vars[abs(varno)].clauses.push_back(snum);
+				fin >> varno;
+			}
+	//		cout << "clause " << snum << " has " << Clauses[snum].vars.size() << " variables" << endl;
+//			logout << "clause " << snum << " has " << Clauses[snum].vars.size() << " variables" << endl;
+			Clauses[snum].active_vars = Clauses[snum].vars.size();
+		}
+	}
+	fin.close();
+}
---
+++ variable_pick.h
@@ -0,0 +1,5 @@
+#pragma once
+extern set<int> Active_clauses;
+int random_pick();
+int two_occur();
+int weight_pick();
